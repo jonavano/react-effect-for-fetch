05cf55058e346db6ab5732d4a177634fe2ac4cc6
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-09-01 11:49:04 +0000

    complete core + extension

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,9 +1,52 @@
+import { useEffect, useState, useCallback } from 'react'
+
+
 function AdviceSection() {
+  const url = "https://api.adviceslip.com/advice";
+
+  const [advice, setAdvice] = useState('');
+  const [favoriteAdvice, setFavoriteAdvice] = useState([]);
+
+
+     const fetchData = useCallback(async () => {
+    const response = await fetch(url, { cache: "no-cache" });
+    const jsonData = await response.json();
+    setAdvice(jsonData.slip.advice);
+  }, [url]);
+
+  function addToFavorites(){
+    setFavoriteAdvice([...favoriteAdvice, advice]);
+  }
+
+  function refreshAdvice() {
+    fetchData();
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData])
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>{advice}</p>
+        <button onClick={refreshAdvice}>Get More Advice</button>
+        <button onClick={addToFavorites}>Save to Favourties</button>
+
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          {/* {console.log("before map")}
+          {console.log(favoriteAdvice)} */}
+          {favoriteAdvice.map((advice) => (
+            <><li>{advice}</li></>
+          ))}
+        </ul>
+
+      </section>
     </section>
   )
 }
--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,61 @@
+import { useEffect, useState } from 'react'
+
 function ArtsSection() {
+  const url = "https://boolean-uk-api-server.fly.dev/art";
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      // console.log(jsonData)
+      setData(jsonData);
+    };
+    fetchData();
+  }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+      <ul className="art-list">
+        {/* {console.log(data)} */}
+        {data.map((section) => (
+          <>
+           <li>
+            
+             <div className="frame">
+             <img
+             src={"https://boolean-uk-api-server.fly.dev" + section.imageURL }
+             />
+             </div>
+            <h3>{section.title}</h3>
+            <p>{section.artist}</p>
+            <h4>Pubication History:</h4>
+            <ul>
+              {section.publicationHistory.map((publication) => (
+                <>
+                <li>
+                  {publication}
+                </li>
+                </>
+              ))}
+
+            </ul>
+
+            
+          
+        
+        </li>
+          
+          </>
+
+        ))}
+
+
+      </ul>
+      </div>
     </section>
   )
 }
